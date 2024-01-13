2e72841d8cdff75400e2343fd1dd2a07ed106e7f
Yat Fu Chan <chanyatfu0616@gmail.com>
2024-01-14 01:00:58 +0800

    Updated TipsPage

--- src/page/TipsPage.tsx
+++ src/page/TipsPage.tsx
@@ -2,15 +2,18 @@ import { Box, Text } from "ink";
 
 export default function TipsPage() {
   const tips = [
-    "1. Press C to force clear a bottle. Press Z to revert.",
-    "2. Press the corresponding number key to switch page.",
-    "3. Your history is automatically saved. You don't need to worry about losing data.",
+    "Press the corresponding number key to switch page.",
+    "Press C to force clear a bottle. Press Z to revert.",
+    "Your history is automatically saved. You don't need to worry about losing data.",
+    "Press Ctrl+C to exit the app.",
+    "Water count toward target only if it have been confirmed",
+    "Use Up/Down arrow to navigate throught setting. Press Enter to select and unselected. Use Up/Down arrow to modified the value",
   ]
 
   return (
     <>
       <Box flexDirection="column" rowGap={1}>
-        {tips.map((tip, index) => <Box key={index}><Text>{tip}</Text></Box>)}
+        {tips.map((tip, index) => <Box key={index}><Text>{index+1}. {tip}</Text></Box>)}
       </Box>
     </>
   )
