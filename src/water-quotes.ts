19c0c02b1cd8daa0259d78cf3d557c6a87920275
Yat Fu Chan <chanyatfu0616@gmail.com>
2024-01-13 17:20:41 +0800

    updated water-quote

--- src/water-quotes.ts
+++ src/water-quotes.ts
@@ -1,3 +1,5 @@
 export const waterQuotes = [
-  'Be like water, my friend.'
+  'Be like water, my friend.',
+  'Thousands have lived without love, not one without water.',
+  'The cure for anything is salt water: sweat, tears or the sea.',
 ]
