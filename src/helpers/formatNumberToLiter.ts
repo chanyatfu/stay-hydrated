b213b3b30c24caa9d8e39538a1f65500dcb3ba74
Yat Fu Chan <chanyatfu0616@gmail.com>
2024-01-13 22:50:29 +0800

    Added emptied to formatted string

--- src/helpers/formatNumberToLiter.ts
+++ src/helpers/formatNumberToLiter.ts
@@ -1,6 +1,8 @@
 export function formatNumberToLiter(volume: number) {
   if (volume >= 1000) {
     return `${Math.round(volume / 10) / 100}L`
+  } else if (volume <= 0) {
+    return 'emptied'
   } else {
     return `${Math.round(volume)}mL`
   }
